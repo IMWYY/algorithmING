e6ebe9f5523622390401cdcdb59437da304ad260
stephac <478290192@qq.com>
2018-05-18 13:13:21 +0800

    add: problem 17 240

---
+++ src/leetCode/Problem017.java
@@ -0,0 +1,42 @@
+package leetCode;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Given a string containing digits from 2-9 inclusive,
+ * return all possible letter combinations that the number could represent.
+ * <p>
+ * A mapping of digit to letters (just like on the telephone buttons) is given below.
+ * Note that 1 does not map to any letters.
+ * <p>
+ * Example:
+ * Input: "23"
+ * Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+ * <p>
+ * create by stephen on 2018/5/18
+ */
+public class Problem017 {
+
+
+    /**
+     * 每一轮将原来list所有的string都加上新字母
+     */
+    public List<String> letterCombinations(String digits) {
+        LinkedList<String> result = new LinkedList<>();
+        if (digits == null || digits.length() == 0) return result;
+        result.add("");
+        String[] letters = {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        for (int i = 0; i < digits.length(); ++i) {
+            String letter = letters[digits.charAt(i)-'0'];
+            while (result.peek().length() == i) {
+                String removed = result.remove();
+                for (char c: letter.toCharArray()) {
+                    result.add(removed + c);
+                }
+            }
+        }
+
+        return result;
+    }
+}
---
+++ src/leetCode/Problem240.java
@@ -0,0 +1,73 @@
+package leetCode;
+
+import java.util.Arrays;
+
+/**
+ * Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+ * <p>
+ * Integers in each row are sorted in ascending from left to right.
+ * Integers in each column are sorted in ascending from top to bottom.
+ * Consider the following matrix:
+ * <p>
+ * [
+ * [1,   4,  7, 11, 15],
+ * [2,   5,  8, 12, 19],
+ * [3,   6,  9, 16, 22],
+ * [10, 13, 14, 17, 24],
+ * [18, 21, 23, 26, 30]
+ * ]
+ * <p>
+ * Example 1:
+ * Input: matrix, target = 5
+ * Output: true
+ * <p>
+ * Example 2:
+ * Input: matrix, target = 20
+ * Output: false
+ * <p>
+ * create by stephen on 2018/5/18
+ */
+public class Problem240 {
+
+    public static void main(String[] args) {
+    }
+
+    /**
+     * 从右上角开始查找
+     * O(m+n) time + O(1) space
+     */
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix.length == 0 || matrix[0].length == 0) return false;
+        int x = 0, y = matrix[0].length - 1;
+
+        while (x <= matrix.length && y >=0) {
+            if (matrix[x][y] == target) return true;
+            if (matrix[x][y] > target) y--;
+            else if (matrix[x][y] < target) x ++;
+        }
+        return false;
+    }
+
+    /**
+     * 利用二分查找
+     * O(mlogn) time + O(1) space
+     */
+    public boolean searchMatrix2(int[][] matrix, int target) {
+        if (matrix.length == 0 || matrix[0].length == 0) return false;
+        int x = matrix.length - 1, y = 0;
+
+        while (x >= 0) {
+            if (matrix[x][y] > target) {
+                x--;
+            } else if (matrix[x][y] == target) {
+                return true;
+            } else {
+                int pos = Arrays.binarySearch(matrix[x], target);
+                if (pos >= 0) return true;
+                x--;
+            }
+        }
+
+        return false;
+    }
+}
