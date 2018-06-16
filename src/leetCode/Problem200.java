4fb8d9fb470eb8accf0617b02686ef6cbedc9f18
stephec <478290192@qq.com>
2018-06-16 09:25:55 +0800

    🚧 work in progress:problem 200

---
+++ src/leetCode/Problem200.java
@@ -0,0 +1,42 @@
+package leetCode;
+
+/**
+ * Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. An island is
+ * surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may
+ * assume all four edges of the grid are all surrounded by water.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input:
+ * 11110
+ * 11010
+ * 11000
+ * 00000
+ * <p>
+ * Output: 1
+ * create by stephen on 2018/6/16
+ */
+public class Problem200 {
+
+    public int numIslands(char[][] grid) {
+        int result = 0;
+        for (int i = 0; i < grid.length; ++i) {
+            for (int j = 0; j < grid[0].length; ++j) {
+                if (grid[i][j] == '1') {
+                    result++;
+                    dfs(i, j, grid);
+                }
+            }
+        }
+        return result;
+    }
+
+    private void dfs(int i, int j, char[][] grid) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] != '1') return;
+        grid[i][j] = 'x';
+        dfs(i + 1, j, grid);
+        dfs(i - 1, j, grid);
+        dfs(i, j + 1, grid);
+        dfs(i, j - 1, grid);
+    }
+}
