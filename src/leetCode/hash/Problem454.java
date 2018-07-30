e86964b041d759dc44b138989199e62e0c676932
stephec <478290192@qq.com>
2018-07-30 09:10:59 +0800

    🚧 work in progress: problem 454

---
+++ src/leetCode/hash/Problem454.java
@@ -0,0 +1,54 @@
+package leetCode.hash;
+
+import java.util.HashMap;
+
+/**
+ * Given four lists A, B, C, D of integer values, compute how many tuples (i, j, k, l) there are such that A[i] + B[j] + C[k] + D[l] is zero.
+ * <p>
+ * To make problem a bit easier, all A, B, C, D have same length of N where 0 ≤ N ≤ 500. All integers are in the range of -228 to 228 - 1 and the result is guaranteed to be at most 231 - 1.
+ * <p>
+ * Example:
+ * <p>
+ * Input:
+ * A = [ 1, 2]
+ * B = [-2,-1]
+ * C = [-1, 2]
+ * D = [ 0, 2]
+ * <p>
+ * Output:
+ * 2
+ * <p>
+ * Explanation:
+ * The two tuples are:
+ * 1. (0, 0, 0, 1) -> A[0] + B[0] + C[0] + D[1] = 1 + (-2) + (-1) + 2 = 0
+ * 2. (1, 1, 0, 0) -> A[1] + B[1] + C[0] + D[0] = 2 + (-1) + (-1) + 0 = 0
+ * create by stephen on 2018/7/30
+ */
+public class Problem454 {
+
+    public static void main(String[] args) {
+        System.out.println(new Problem454().fourSumCount(new int[]{1,2}, new int[]{-2, -1}, new int[]{-1, 2}, new int[]{0, 2}));
+    }
+
+
+    public int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
+        int res = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int aA : A) {
+            for (int aB : B) {
+                int t = aA + aB;
+                map.put(t, map.getOrDefault(t, 0) + 1);
+            }
+        }
+
+        for (int c : C) {
+            for (int d : D) {
+                int t = c + d;
+                res += map.getOrDefault(-t, 0);
+            }
+        }
+        return res;
+    }
+
+}
