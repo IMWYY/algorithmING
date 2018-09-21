d193eec1a66f95e06249a205a6fd62633bd0adf5
stephen.wang <stephen.wang@bytedance.com>
2018-09-21 20:32:55 +0800

    add problem 029
    Change-Id: Idfb2a53f1ba55cd0f3dfc66bd87c44f3331c4ca0

--- src/leetCode/math/Problem029.java
+++ src/leetCode/math/Problem029.java
@@ -20,37 +20,76 @@ package leetCode.math;
  */
 public class Problem029 {
 
-    public static void main(String[] args) {
-        System.out.println(divide(Integer.MIN_VALUE, -3));
-    }
+	public static void main(String[] args) {
+		System.out.println(divide(-2147483648,2));
+	}
 
-    public static int divide(int dividend, int divisor) {
-        if (divisor == 1) return dividend;
-        if (dividend == divisor) return 1;
-        if (divisor == Integer.MIN_VALUE) {
-            return 0;
-        }
+	// 利用位操作 很巧妙
+	// 参见 https://leetcode.com/problems/divide-two-integers/discuss/13407/Detailed-Explained-8ms-C++-solution
+	public static int divide(int dividend, int divisor) {
+		if (dividend == Integer.MIN_VALUE && divisor == -1) {
+			return Integer.MAX_VALUE;
+		}
+		if (divisor == 1) {
+			return dividend;
+		} else if (divisor == -1) {
+			return -dividend;
+		}
+		int sign = ((dividend < 0) ^ (divisor < 0)) ? -1 : 1;
+		// 需要注意这里应该用long 处理int_min的溢出情况
+		long dvd = Math.abs((long) dividend);
+		System.out.println(dvd);
+		long dvs = Math.abs((long) divisor);
 
-        boolean isPos = true;
-        if ((dividend > 0 && divisor < 0) || (dividend < 0 && divisor > 0)) {
-            isPos = false;
-        }
-        int res = 0;
-        boolean canGoOn = true;
-        while ((dividend > 0 && dividend - divisor > 0)) {
-            if (isPos) {
-                if (res == Integer.MAX_VALUE) {
+		int res = 0;
+		while (dvd >= dvs) {
+			long temp = dvs, multiple = 1;
+			while (dvd >= (temp << 1)) {
+				temp <<= 1;
+				multiple <<= 1;
+			}
+			dvd -= temp;
+			res += multiple;
+		}
+		return sign == 1 ? res : -res;
+	}
 
-                }
-                res++;
-            } else {
-                if (res == Integer.MIN_VALUE) {
-                    return Integer.MAX_VALUE;
-                }
-                res--;
-            }
-            dividend -= divisor;
-        }
-        return res;
-    }
+	// 考虑了边界条件 但是时间复杂度太高
+	public int divide1(int dividend, int divisor) {
+		if (divisor == 1)
+			return dividend;
+		if (dividend == divisor)
+			return 1;
+		if (divisor == Integer.MIN_VALUE) {
+			return 0;
+		}
+
+		boolean isPos = true;
+		if ((dividend > 0 && divisor < 0) || (dividend < 0 && divisor > 0)) {
+			isPos = false;
+		}
+		int res = 0;
+		while ((dividend > 0 && divisor > 0 && dividend - divisor >= 0) ||
+				(dividend > 0 && divisor < 0 && dividend + divisor >= 0) ||
+				(dividend < 0 && divisor > 0 && dividend + divisor <= 0) ||
+				(dividend < 0 && divisor < 0 && dividend - divisor <= 0)) {
+			if (isPos) {
+				if (res == Integer.MAX_VALUE) {
+					return Integer.MAX_VALUE;
+				}
+				res++;
+			} else {
+				if (res == Integer.MIN_VALUE) {
+					return Integer.MAX_VALUE;
+				}
+				res--;
+			}
+			if ((dividend > 0 && divisor > 0 || (dividend < 0 && divisor < 0))) {
+				dividend -= divisor;
+			}  else {
+				dividend += divisor;
+			}
+		}
+		return res;
+	}
 }
