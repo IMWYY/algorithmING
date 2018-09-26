49003764760ae55c1bf228c1c2f0b2d9906ea39b
stephec <478290192@qq.com>
2018-09-26 09:36:15 +0800

    fucking problem227

--- src/leetCode/string/Problem227.java
+++ src/leetCode/string/Problem227.java
@@ -25,11 +25,6 @@ public class Problem227 {
     }
 
     public static int calculate(String s) {
-        return calculate(s, false);
-
-    }
-
-    public static int calculate(String s, boolean negative) {
         s = s.trim();
         if (s.equals("")) return 0;
 
@@ -40,17 +35,10 @@ public class Problem227 {
             if (c == ' ') {
                 i++;
             } else if (c == '+') {
-                System.out.println(negative + " " + operandA +  " + ");
-                if (negative) {
-                    return operandA - calculate(s.substring(i + 1), true);
-                }
-                return operandA + calculate(s.substring(i + 1), false);
+                return operandA + calculate(s.substring(i + 1));
             } else if (c == '-') {
-                System.out.println(negative + " " + operandA + " - " );
-                if (negative) {
-                    return operandA - calculate(s.substring(i + 1), true);
-                }
-                return operandA - calculate(s.substring(i + 1), true);
+                String temp = s.substring(i + 1);
+                return operandA - calculate();
             } else if (c == '*') {
                 isMulti = true;
                 i++;
@@ -83,4 +71,59 @@ public class Problem227 {
         }
         return operandA;
     }
+
+//    public static int calculate(String s, boolean negative) {
+//        s = s.trim();
+//        if (s.equals("")) return 0;
+//
+//        int operandA = -1, operandB = -1;
+//        boolean isMulti = false, isDivison = false;
+//        for (int i = 0; i < s.length(); ) {
+//            char c = s.charAt(i);
+//            if (c == ' ') {
+//                i++;
+//            } else if (c == '+') {
+//                System.out.println(negative + " " + operandA +  " + ");
+//                if (negative) {
+//                    return operandA - calculate(s.substring(i + 1), true);
+//                }
+//                return operandA + calculate(s.substring(i + 1), false);
+//            } else if (c == '-') {
+//                System.out.println(negative + " " + operandA + " - " );
+//                if (negative) {
+//                    return operandA - calculate(s.substring(i + 1), true);
+//                }
+//                return operandA - calculate(s.substring(i + 1), true);
+//            } else if (c == '*') {
+//                isMulti = true;
+//                i++;
+//            } else if (c == '/') {
+//                isDivison = true;
+//                i++;
+//            } else {
+//                int start = i;
+//                while (c >= '0' && c <= '9') {
+//                    i++;
+//                    if (i >= s.length()) break;
+//                    c = s.charAt(i);
+//                }
+//                int num = Integer.valueOf(s.substring(start, i));
+//                if (operandA == -1) {
+//                    operandA = num;
+//                } else if (operandB == -1) {
+//                    operandB = num;
+//                    if (isMulti) {
+//                        operandA *= operandB;
+//                        operandB = -1;
+//                        isMulti = false;
+//                    } else if (isDivison) {
+//                        operandA /= operandB;
+//                        operandB = -1;
+//                        isDivison = false;
+//                    }
+//                }
+//            }
+//        }
+//        return operandA;
+//    }
 }
