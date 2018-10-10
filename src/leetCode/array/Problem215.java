7bac198189dec1cf00eea8d28b1080c65ad360bf
stephec <478290192@qq.com>
2018-10-10 08:59:43 +0800

    add problem 324

--- src/leetCode/array/Problem215.java
+++ src/leetCode/array/Problem215.java
@@ -1,5 +1,8 @@
 package leetCode.array;
 
+import java.util.PriorityQueue;
+import java.util.Random;
+
 /**
  * Find the kth largest element in an unsorted array. Note that it is the kth largest element in the sorted order, not the kth distinct element.
  * <p>
@@ -14,7 +17,78 @@ package leetCode.array;
  * create by stephen on 2018/10/9
  */
 public class Problem215 {
+
+    /**
+     * 利用最小堆 并保存堆大小为k
+     */
     public int findKthLargest(int[] nums, int k) {
-        return 0;
+        if (k == 0 || nums.length == 0) return 0;
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for (int i : nums) {
+            if (minHeap.size() < k) {
+                minHeap.add(i);
+            } else {
+                if (minHeap.peek() < i) {
+                    minHeap.poll();
+                    minHeap.add(i);
+                }
+            }
+        }
+        return minHeap.peek();
+    }
+
+    /**
+     * 利用选择排序的思想
+     * randomize the input 来保证时间复杂度为O(n)
+     */
+    public int findKthLargest1(int[] nums, int k) {
+        shuffle(nums);
+        k = nums.length - k;
+        int lo = 0;
+        int hi = nums.length - 1;
+        while (lo < hi) {
+            final int j = partition(nums, lo, hi);
+            if (j < k) {
+                lo = j + 1;
+            } else if (j > k) {
+                hi = j - 1;
+            } else {
+                break;
+            }
+        }
+        return nums[k];
+    }
+
+    // randomize the input
+    private void shuffle(int a[]) {
+        final Random random = new Random();
+        for (int ind = 1; ind < a.length; ind++) {
+            final int r = random.nextInt(ind + 1);
+            exch(a, ind, r);
+        }
+    }
+
+    /**
+     * 每次执行完partition之后 j位置前的数小于num[j] j位置后的数大于num[j]
+     */
+    private int partition(int[] a, int lo, int hi) {
+        int i = lo;
+        int j = hi + 1;
+        while (true) {
+            while (i < hi && a[++i] < a[lo]) ;
+            while (j > lo && a[lo] < a[--j]) ;
+            if (i >= j) {
+                break;
+            }
+            exch(a, i, j);
+        }
+        exch(a, lo, j);
+        return j;
+    }
+
+    private void exch(int[] a, int i, int j) {
+        final int tmp = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
     }
 }
--- src/leetCode/array/Problem324.java
+++ src/leetCode/array/Problem324.java
@@ -1,6 +1,7 @@
 package leetCode.array;
 
 import java.util.Arrays;
+import java.util.Random;
 
 /**
  * Given an unsorted array nums, reorder it such that nums[0] < nums[1] > nums[2] < nums[3]....
@@ -22,11 +23,14 @@ public class Problem324 {
      * <p>
      * 可以证明 局部的大小关系经过转换后可以得到一个全局的大小关系：odd index的数字不小于even index的数字
      * 所以将数组排序后，分成两部分，小数字部分放到even index，大数字部分放到odd index就可以了
+     * j = 2 * (m - 1 - i) if i < m
+     * j = 2 * (n - 1 - i) + 1 if i >= m
      * <p>
      * 但是需要考虑数字相等的情况，不能让相等的数组相邻，
      * 所以在放的时候，对于小数字部分，将较大的数字放在index小的地方；对于大数字部分，将较小的数字放在index大的地方，这样相等的部分就不会相邻了
+     * O(nlogn) time and O(n) space
      */
-    public void wiggleSort(int[] nums) {
+    public void wiggleSort1(int[] nums) {
         int n = nums.length, m = (n + 1) >> 1;
         int[] copy = Arrays.copyOf(nums, n);
         Arrays.sort(copy);
@@ -37,5 +41,88 @@ public class Problem324 {
         for (int i = n - 1, j = 1; i >= m; i--, j += 2) {
             nums[j] = copy[i];
         }
+    }
+
+    /**
+     * 优化寻找中间数字的过程 参考Problem215 同时使用一个额外数组
+     * O(n) time and O(n) space
+     */
+    public void wiggleSort2(int[] nums) {
+        int n = nums.length, m = (n + 1) >> 1;
+        int[] copy = Arrays.copyOf(nums, n);
+        int median = kthSmallestNumber(nums, m);
+
+        for (int i = 0, j = 0, k = n - 1; j <= k;) {
+            if (copy[j] < median) {
+                swap(copy, i++, j++);
+            } else if (copy[j] > median) {
+                swap(copy, j, k--);
+            } else {
+                j++;
+            }
+        }
+
+        for (int i = m - 1, j = 0; i >= 0; i--, j += 2) nums[j] = copy[i];
+        for (int i = n - 1, j = 1; i >= m; i--, j += 2) nums[j] = copy[i];
+    }
+
+
+    /**
+     * 优化寻找中间数字的过程 参考Problem215
+     * O(n) time and O(1) space
+     */
+    public void wiggleSort3(int[] nums) {
+        int n = nums.length, m = (n + 1) >> 1;
+        // 采用Problem215的最优算法 O(n) time and O(1) space
+        int median = kthSmallestNumber(nums, m);
+
+        for (int i = 0, j = 0, k = n - 1; j <= k; ) {
+            if (nums[A(j, n)] > median) {
+                swap(nums, A(i++, n), A(j++, n));
+            } else if (nums[A(j, n)] < median) {
+                swap(nums, A(j, n), A(k--, n));
+            } else {
+                j++;
+            }
+        }
+    }
+
+    private int A(int i, int n) {
+        return (2 * i + 1) % (n | 1);
+    }
+
+    private int kthSmallestNumber(int[] nums, int k) {
+        Random random = new Random();
+        for (int i = nums.length - 1; i >= 0; i--) {
+            swap(nums, i, random.nextInt(i + 1));
+        }
+        int l = 0, r = nums.length - 1;
+        k--;
+        while (l < r) {
+            int m = getMiddle(nums, l, r);
+            if (m < k) {
+                l = m + 1;
+            } else if (m > k) {
+                r = m - 1;
+            } else {
+                break;
+            }
+        }
+        return nums[k];
+    }
+
+    private int getMiddle(int[] nums, int l, int r) {
+        int i = l;
+        for (int j = l + 1; j <= r; j++) {
+            if (nums[j] < nums[l]) swap(nums, ++i, j);
+        }
+        swap(nums, l, i);
+        return i;
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int t = nums[i];
+        nums[i] = nums[j];
+        nums[j] = t;
     }
 }
