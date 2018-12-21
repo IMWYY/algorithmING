6d5fe8e4b5313c24ffe97ca8f632ea33a3e01c93
stephen.wang <stephen.wang@bytedance.com>
2018-12-21 09:19:16 +0800

    review array
    Change-Id: If11be4737a27ecf7a24f65f4bd9fc8a85cf9a5da

---
+++ src/leetCode/binarySearch/Problem035.java
@@ -0,0 +1,39 @@
+package leetCode.array;
+
+/**
+ * Given a sorted array and a target value, return the index if the target is found.
+ * If not, return the index where it would be if it were inserted in order.
+ * <p>
+ * You may assume no duplicates in the array.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: [1,3,5,6], 5
+ * Output: 2
+ * create by stephen on 2018/6/21
+ */
+public class Problem035 {
+
+	/**
+	 * 几种二分查找要区分一下：
+	 * 等值查找 第一次出现 最后一次出现 第一个比target小的 第一个比target大的
+	 */
+	public int searchInsert(int[] nums, int target) {
+		if (nums.length == 0)
+			return 0;
+		int left = 0, right = nums.length - 1;
+
+		while (left <= right) {
+			int mid = left + ((right - left) >> 1);
+
+			if (nums[mid] == target) {
+				return mid;
+			} else if (nums[mid] < target) {
+				left = mid + 1;
+			} else {
+				right = mid - 1;
+			}
+		}
+		return left;
+	}
+}
