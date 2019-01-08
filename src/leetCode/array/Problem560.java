da4e4b7bb7452fcb361048d78aac58b354944cab
stephen.wang <stephen.wang@bytedance.com>
2019-01-08 10:07:56 +0800

    reivew array 560
    Change-Id: Ie2e1d09cbf63898b58cac6a041606321a709b6aa

--- src/leetCode/array/Problem560.java
+++ src/leetCode/array/Problem560.java
@@ -15,85 +15,87 @@ import java.util.HashMap;
  */
 public class Problem560 {
 
-    /**
-     * 遍历一遍 利用HashMap记录sum的对应出现的次数
-     * 每次操作map查看sum-k是否在map中 count加上出现的次数
-     * 举例说明：
-     * 加入当前下标为i，sum(i)-k在map中说明，之前的某个位置j sum(j) = sum(i)-k
-     * 也即 sum(i)-sum(j) = k 就是 i-j的和等于k
-     * O(n) time + O(n) space
-     */
-    public int subarraySum(int[] nums, int k) {
-        int count = 0, sum = 0;
-        HashMap<Integer, Integer> map = new HashMap<>();
-        map.put(0, 1);
-        for (int i = 0; i < nums.length; i++) {
-            sum += nums[i];
-            if (map.containsKey(sum - k))
-                count += map.get(sum - k);
-            map.put(sum, map.getOrDefault(sum, 0) + 1);
-        }
-        return count;
-    }
+	/**
+	 * 遍历一遍 利用HashMap记录sum的对应出现的次数
+	 * 每次操作map查看sum-k是否在map中，如果在那么count加上sum-k出现的次数
+	 * 举例说明：
+	 * 加入当前下标为i，sum(i)-k在map中说明，之前的某个位置j sum(j) = sum(i)-k
+	 * 也即 sum(i)-sum(j) = k 就是 i-j的和等于k
+	 * O(n) time + O(n) space
+	 */
+	public int subarraySum(int[] nums, int k) {
+		int count = 0, sum = 0;
+		HashMap<Integer, Integer> map = new HashMap<>();
+		map.put(0, 1);
+		for (int num : nums) {
+			sum += num;
+			if (map.containsKey(sum - k))
+				count += map.get(sum - k);
+			map.put(sum, map.getOrDefault(sum, 0) + 1);
+		}
+		return count;
+	}
 
-    /**
-     * 动态规划解法
-     * O(n2) time + O(n) space
-     */
-    public int subarraySum1(int[] nums, int k) {
-        if (nums.length == 0) return 0;
-        int[] dp = new int[nums.length];
-        Arrays.fill(dp, 0);
-        dp[0] = k == nums[0] ? 1 : 0;
-        int temp;
-        for (int i = 1; i < nums.length; ++i) {
-            temp = 0;
-            dp[i] += dp[i - 1];
-            for (int j = i; j >= 0; j--) {
-                temp += nums[j];
-                if (temp == k) dp[i]++;
-            }
-        }
+	/**
+	 * 动态规划解法
+	 * dp[i] 表示数组前i位的连续子数组的和为k的个数
+	 * 那么dp[i+1] = dp[i] + (以i+1结尾的连续子数组和为k的个数)
+	 * O(n2) time + O(n) space
+	 */
+	public int subarraySum1(int[] nums, int k) {
+		if (nums.length == 0)
+			return 0;
+		int[] dp = new int[nums.length];
+		Arrays.fill(dp, 0);
+		dp[0] = k == nums[0] ? 1 : 0;
+		int temp;
+		for (int i = 1; i < nums.length; ++i) {
+			temp = 0;
+			dp[i] += dp[i - 1];
+			for (int j = i; j >= 0; j--) {
+				temp += nums[j];
+				if (temp == k)
+					dp[i]++;
+			}
+		}
 
-        return dp[nums.length - 1];
-    }
+		return dp[nums.length - 1];
+	}
 
+	/**
+	 * 记录累加和 那么[i,j]之间的和为sum[j]-sum[i]
+	 * O(n2) time + O(1) space
+	 */
+	public int subarraySum2(int[] nums, int k) {
+		int count = 0;
+		int[] sum = new int[nums.length + 1];
+		sum[0] = 0;
+		for (int i = 1; i <= nums.length; i++)
+			sum[i] = sum[i - 1] + nums[i - 1];
+		for (int start = 0; start < nums.length; start++) {
+			for (int end = start + 1; end <= nums.length; end++) {
+				if (sum[end] - sum[start] == k)
+					count++;
+			}
+		}
+		return count;
+	}
 
-    /**
-     * 记录类加和 那么[i,j]之间的和为sum[j]-sum[i]
-     * O(n2) time + O(1) space
-     */
-    public int subarraySum2(int[] nums, int k) {
-        int count = 0;
-        int[] sum = new int[nums.length + 1];
-        sum[0] = 0;
-        for (int i = 1; i <= nums.length; i++)
-            sum[i] = sum[i - 1] + nums[i - 1];
-        for (int start = 0; start < nums.length; start++) {
-            for (int end = start + 1; end <= nums.length; end++) {
-                if (sum[end] - sum[start] == k)
-                    count++;
-            }
-        }
-        return count;
-    }
-
-    /**
-     * 不用预处理 直接比较
-     * O(n2) time + O(1) space
-     */
-    public int subarraySum3(int[] nums, int k) {
-        int count = 0;
-        for (int start = 0; start < nums.length; start++) {
-            int sum = 0;
-            for (int end = start; end < nums.length; end++) {
-                sum += nums[end];
-                if (sum == k)
-                    count++;
-            }
-        }
-        return count;
-    }
-
+	/**
+	 * 不用预处理 直接比较
+	 * O(n2) time + O(1) space
+	 */
+	public int subarraySum3(int[] nums, int k) {
+		int count = 0;
+		for (int start = 0; start < nums.length; start++) {
+			int sum = 0;
+			for (int end = start; end < nums.length; end++) {
+				sum += nums[end];
+				if (sum == k)
+					count++;
+			}
+		}
+		return count;
+	}
 
 }
