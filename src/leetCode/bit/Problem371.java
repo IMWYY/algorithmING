13b29576b0e6b4964baaaa9b81dff389b96da80b
stephen.wang <stephen.wang@bytedance.com>
2019-01-14 12:54:08 +0800

    review bit 371
    Change-Id: Ibb2c310c0f2dda20539cde54a99c302ab3698b2b

--- src/leetCode/bit/Problem371.java
+++ src/leetCode/bit/Problem371.java
@@ -10,22 +10,21 @@ package leetCode.bit;
  * create by stephen on 2018/10/12
  */
 public class Problem371 {
-
-    /**
-     * a&b找到两个数相同的部分 所以要左移一位（x2）
-     * a^b找到两个数不同的部分
-     * 直到没有相同部分（b=0） 直接返回a
-     * 一个例子解释：https://leetcode.com/problems/sum-of-two-integers/discuss/132479/Simple-explanation-on-how-to-arrive-at-the-solution
-     *
-     * 当a和b的所有对应位置的1和0不重复时 a^b可以得到a+b, 但是当有两个1时，就会有进位问题。
-     */
-    public int getSum(int a, int b) {
-        if (a == 0) return b;
-        while (b != 0) {
-            int carry = a & b;
-            a = a ^ b;
-            b = carry << 1;
-        }
-        return a;
-    }
+	/**
+	 * 一个例子解释：https://leetcode.com/problems/sum-of-two-integers/discuss/132479/Simple-explanation-on-how-to-arrive-at-the-solution
+	 * <p>
+	 * 当a+b没有进位的时候，a^b可以得到a+b的结果
+	 * 当有进入的时候，a&b可以拿到所有的进位，进位需要左移（进位上1）
+	 * 继续循环 知道进位为0 则结果a+b = a^b
+	 */
+	public int getSum(int a, int b) {
+		if (a == 0)
+			return b;
+		while (b != 0) {
+			int carry = a & b;
+			a = a ^ b;
+			b = carry << 1;
+		}
+		return a;
+	}
 }
