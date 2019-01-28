50024620d2da02ae8c4e729de5948d683930f454
stephen.wang <stephen.wang@bytedance.com>
2019-01-28 10:12:25 +0800

    review dp 62 64 readme
    Change-Id: I87ccfde6cb88f85d58c4a99bd288999b19bbe5ed

--- src/leetCode/dynamicProgramming/Problem062.java
+++ src/leetCode/dynamicProgramming/Problem062.java
@@ -1,5 +1,7 @@
 package leetCode.dynamicProgramming;
 
+import java.util.Arrays;
+
 /**
  * A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
  * <p>
@@ -11,22 +13,37 @@ package leetCode.dynamicProgramming;
  */
 public class Problem062 {
 
-    /**
-     * 动态规划简单题 可减少空间复杂度到O(n)
-     */
-    public int uniquePaths(int m, int n) {
-        Integer[][] map = new Integer[m][n];
-        for (int i = 0; i < m; i++) {
-            map[i][0] = 1;
-        }
-        for (int j = 0; j < n; j++) {
-            map[0][j] = 1;
-        }
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                map[i][j] = map[i - 1][j] + map[i][j - 1];
-            }
-        }
-        return map[m - 1][n - 1];
-    }
+	/**
+	 * 动态规划简单题 减少空间复杂度到O(n)
+	 * O(n^2) time + O(n) space
+	 */
+	public int uniquePaths(int m, int n) {
+		int[] dp = new int[n];
+		Arrays.fill(dp, 1);
+		for (int i = 1; i < m; i++) {
+			for (int j = 1; j < n; j++) {
+				dp[j] = dp[j - 1] + dp[j];
+			}
+		}
+		return dp[n - 1];
+	}
+
+	/**
+	 * O(n^2) time + O(n^2) space
+	 */
+	public int uniquePaths1(int m, int n) {
+		int[][] map = new int[m][n];
+		for (int i = 0; i < m; i++) {
+			map[i][0] = 1;
+		}
+		for (int j = 0; j < n; j++) {
+			map[0][j] = 1;
+		}
+		for (int i = 1; i < m; i++) {
+			for (int j = 1; j < n; j++) {
+				map[i][j] = map[i - 1][j] + map[i][j - 1];
+			}
+		}
+		return map[m - 1][n - 1];
+	}
 }
--- src/leetCode/dynamicProgramming/Problem064.java
+++ src/leetCode/dynamicProgramming/Problem064.java
@@ -20,34 +20,29 @@ package leetCode.dynamicProgramming;
  */
 public class Problem064 {
 
-    /**
-     * 典型动态规划解法 也可以直接在原数组上修改 这样可以节省空间
-     */
-    public int minPathSum(int[][] grid) {
-        if (grid.length == 0) {
-            return 0;
-        }
+	/**
+	 * 典型动态规划解法 也可以直接在原数组上修改 这样可以节省空间
+	 * O(n^2) time + O(n^2) space
+	 */
+	public int minPathSum(int[][] grid) {
+		if (grid.length == 0) {
+			return 0;
+		}
 
-        int[][] dp = new int[grid.length][grid[0].length];
-        int temp = 0;
-        for (int i = 0; i < dp[0].length; ++i) {
-            temp += grid[0][i];
-            dp[0][i] = temp;
-        }
+		int[][] dp = new int[grid.length][grid[0].length];
+		for (int i = 0; i < dp[0].length; ++i) {
+			dp[0][i] = i > 0 ? dp[0][i - 1] + grid[0][i] : grid[0][0];
+		}
+		for (int i = 0; i < dp.length; ++i) {
+			dp[i][0] = i > 0 ? dp[i - 1][0] + grid[i][0] : grid[0][0];
+		}
 
-        temp = 0;
-        for (int i = 0; i < dp.length; ++i) {
-            temp += grid[i][0];
-            dp[i][0] = temp;
-        }
-
-        for (int i = 1; i < dp.length; ++i) {
-            for (int j = 1; j < dp[0].length; ++j) {
-                dp[i][j] = (Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j]);
-            }
-        }
-
-        return dp[dp.length - 1][dp[0].length - 1];
-    }
+		for (int i = 1; i < dp.length; ++i) {
+			for (int j = 1; j < dp[0].length; ++j) {
+				dp[i][j] = (Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j]);
+			}
+		}
+		return dp[dp.length - 1][dp[0].length - 1];
+	}
 
 }
