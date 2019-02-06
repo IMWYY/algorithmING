d01526cd7f0efe02986a72d1a1f068a88df72f2f
stephec <478290192@qq.com>
2019-02-06 11:23:58 +0800

    finish greedy

--- src/leetCode/greedy/Problem055.java
+++ src/leetCode/greedy/Problem055.java
@@ -27,7 +27,8 @@ public class Problem055 {
 
     /**
      * 贪心算法
-     * 循环找到可以到达重点的最左位置 如果最后lastPos=0 则可以到达重点
+     * 循环找到可以到达终点的最左位置 如果最后lastPos=0 则可以到达重点
+     * O(n) time + O(n) space
      */
     public boolean canJump(int[] nums) {
         int lastPos = nums.length - 1;
@@ -39,25 +40,46 @@ public class Problem055 {
         return lastPos == 0;
     }
 
+    /**
+     * 动态规划 从后往前
+     */
+    public boolean canJump1(int[] nums) {
+        boolean[] memo = new boolean[nums.length];
+        Arrays.fill(memo, false);
+        memo[memo.length - 1] = true;
+
+        for (int i = nums.length - 2; i >= 0; i--) {
+            int furthestJump = Math.min(i + nums[i], nums.length - 1);
+            for (int j = i + 1; j <= furthestJump; j++) {
+                if (memo[j]) {
+                    memo[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return memo[0];
+    }
 
     /**
+     * 动态规划
      * 从前向后遍历 将所有可以到达的下标设为true
      * O(n2) time + O(n) space
      */
-    public boolean canJump1(int[] nums) {
+    public boolean canJump2(int[] nums) {
         if (nums.length <= 1) return true;
         boolean[] reach = new boolean[nums.length];
         Arrays.fill(reach, false);
         reach[0] = true;
-        for (int i=0; i<nums.length; ++i) {
+        for (int i = 0; i < nums.length; ++i) {
             if (!reach[i]) continue;        // 只能从可以到达的位置算
             int canReach = i + nums[i];
-            if (i + nums[i] >= nums.length-1) return true;
-            for (int j=i+1; j<=canReach; ++j) {
+            if (i + nums[i] >= nums.length - 1) return true;
+            for (int j = i + 1; j <= canReach; ++j) {
                 reach[j] = true;
             }
         }
-        return reach[nums.length-1];
+        return reach[nums.length - 1];
     }
 
 
