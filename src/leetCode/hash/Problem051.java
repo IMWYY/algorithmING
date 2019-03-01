9d45690725a7c112f32f5c8daaf0da9307277b1e
stephec <478290192@qq.com>
2019-03-01 08:24:07 +0800

    n queens

--- src/leetCode/hash/Problem051.java
+++ src/leetCode/hash/Problem051.java
@@ -6,6 +6,11 @@ import java.util.HashMap;
 import java.util.List;
 
 /**
+ * The n-queens puzzle is the problem of placing n queens on an n×n chessboard such that no two queens attack each other.
+ * <p>
+ * Given an integer n, return all distinct solutions to the n-queens puzzle.
+ * Each solution contains a distinct board configuration of the n-queens' placement, where 'Q' and '.'
+ * both indicate a queen and an empty space respectively.
  * create by stephen on 2019/3/1
  */
 public class Problem051 {
