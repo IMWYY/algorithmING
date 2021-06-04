49cb7afc2da799d069ebb0484830fe6970da520d
stephen <wyy.stephen@gmail.com>
2021-06-04 17:09:28 +0800

    stuff

--- leetCode/dp/Problem1504.cpp
+++ leetCode/dp/Problem1504.cpp
@@ -7,31 +7,27 @@
 #include <unordered_set>
 #include <vector>
 
-using namespace std;
-
 /**
  * Given a rows * columns matrix mat of ones and zeros, return how many
  * submatrices have all ones.
  */
 
-int numSubmat(vector<vector<int>>& mat) {
+int numSubmat(std::vector<std::vector<int>>& mat) {
   int m = mat.size(), n = mat[0].size();
-  for (int i = m - 1; i >= 0; i--) {
-    for (int j = n - 2; j >= 0; j--) {
-      // mat[i][j] record # of continuous 1 start at [i,j] in the ith row.
-      mat[i][j] = mat[i][j] == 0 ? 0 : mat[i][j] + mat[i][j + 1];
-    }
-  }
-
   int res = 0;
-  for (int i = 0; i < m; ++i) {
-    for (int j = 0; j < n; ++j) {
-      int cols = mat[i][j];
-      res += cols;  // #matrix of 1 x n
-      for (int k = i + 1; k < m; ++k) {
-        cols = std::min(cols, mat[k][j]);
-        if (cols == 0) break;
-        res += cols;  // #matrix of (k - i + 1) x n
+  std::vector<int> heights(n, 0);
+  for (size_t i = 0; i < m; ++i) {
+    for (size_t j = 0; j < n; ++j) {
+      if (mat[i][j])
+        heights[j]++;
+      else
+        heights[j] = 0;
+
+      int h = heights[j];
+      for (int k = j; k >= 0; k--) {
+        h = std::min(heights[k], h);
+        if (h == 0) break;
+        res += h;
       }
     }
   }
