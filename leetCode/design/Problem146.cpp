381bbf17d269836c8cf2d944dabc7b4e10fe6811
stephen <wyy.stephen@gmail.com>
2021-09-25 23:53:47 +0800

    lru

--- leetCode/design/Problem146.cpp
+++ leetCode/design/Problem146.cpp
@@ -1,7 +1,37 @@
+#include <list>
 #include <unordered_map>
 #include <vector>
 
+// lru cache using std::list
 class LRUCache {
+ public:
+  int size;
+  std::list<std::pair<int, int>> lru;
+  std::unordered_map<int, std::list<std::pair<int, int>>::iterator> mp;
+
+  LRUCache(int capacity) : size(capacity) {}
+
+  int get(int key) {
+    if (mp.count(key) == 0) return -1;
+    updateLRU(key, (*mp[key]).second);
+    return (*mp[key]).second;
+  }
+  void put(int key, int value) {
+    if (mp.size() == size && mp.count(key) == 0) evict();
+    updateLRU(key, value);
+  }
+  void updateLRU(int key, int value) {
+    if (mp.count(key) > 0) lru.erase(mp[key]);
+    lru.push_front({key, value});
+    mp[key] = lru.begin();
+  }
+
+  void evict() {
+    mp.erase(lru.back().first);
+    lru.pop_back();
+  }
+};
+class MyLRUCache {
  public:
   struct Node {
     int key;
@@ -11,14 +41,14 @@ class LRUCache {
     Node(int k, int v) : key(k), val(v) {}
   };
 
-  LRUCache(int cap) : sz(0), cap(cap) {
+  MyLRUCache(int cap) : sz(0), cap(cap) {
     head = new Node(-1, -1);
     tail = new Node(-1, -1);
     head->next = tail;
     tail->prev = head;
   }
 
-  ~LRUCache() {
+  ~MyLRUCache() {
     Node* n = head;
     while (n) {
       Node* tmp = n->next;
