b3e0c94a3e8a97870e854ab0d5e816754bbd4854
stephen <wyy.stephen@gmail.com>
2022-05-01 20:56:30 +0800

    update function

--- basics/c++/my_function.cpp
+++ basics/c++/my_function.cpp
@@ -1,117 +1,97 @@
 #include <iostream>
-#include <memory>
 
-template <typename T>
-class function;
-
-template <typename R, typename... Args>
-class function<R(Args...)> {
-  // function pointer types for the type-erasure behaviors
-  // all these char* parameters are actually casted from some functor type
-  typedef R (*invoke_fn_t)(char*, Args&&...);
-  typedef void (*construct_fn_t)(char*, char*);
-  typedef void (*destroy_fn_t)(char*);
-
-  // type-aware generic functions for invoking
-  // the specialization of these functions won't be capable with
-  //   the above function pointer types, so we need some cast
-  template <typename Functor>
-  static R invoke_fn(Functor* fn, Args&&... args) {
-    return (*fn)(std::forward<Args>(args)...);
-  }
-
-  template <typename Functor>
-  static void construct_fn(Functor* construct_dst, Functor* construct_src) {
-    // the functor type must be copy-constructible
-    new (construct_dst) Functor(*construct_src);
-  }
-
-  template <typename Functor>
-  static void destroy_fn(Functor* f) {
-    f->~Functor();
-  }
-
-  // these pointers are storing behaviors
-  invoke_fn_t invoke_f;
-  construct_fn_t construct_f;
-  destroy_fn_t destroy_f;
-
-  // erase the type of any functor and store it into a char*
-  // so the storage size should be obtained as well
-  std::unique_ptr<char[]> data_ptr;
-  size_t data_size;
-
- public:
-  function()
-      : invoke_f(nullptr),
-        construct_f(nullptr),
-        destroy_f(nullptr),
-        data_ptr(nullptr),
-        data_size(0) {}
-
-  // construct from any functor type
-  template <typename Functor>
-  function(Functor f)
-      // specialize functions and erase their type info by casting
-      : invoke_f(reinterpret_cast<invoke_fn_t>(invoke_fn<Functor>)),
-        construct_f(reinterpret_cast<construct_fn_t>(construct_fn<Functor>)),
-        destroy_f(reinterpret_cast<destroy_fn_t>(destroy_fn<Functor>)),
-        data_ptr(new char[sizeof(Functor)]),
-        data_size(sizeof(Functor)) {
-    // copy the functor to internal storage
-    this->construct_f(this->data_ptr.get(), reinterpret_cast<char*>(&f));
-  }
-
-  // copy constructor
-  function(function const& rhs)
-      : invoke_f(rhs.invoke_f),
-        construct_f(rhs.construct_f),
-        destroy_f(rhs.destroy_f),
-        data_size(rhs.data_size) {
-    if (this->invoke_f) {
-      // when the source is not a null function, copy its internal functor
-      this->data_ptr.reset(new char[this->data_size]);
-      this->construct_f(this->data_ptr.get(), rhs.data_ptr.get());
-    }
-  }
-
-  ~function() {
-    if (data_ptr != nullptr) {
-      this->destroy_f(this->data_ptr.get());
-    }
-  }
-
-  // other constructors, from nullptr, from function pointers
-
-  R operator()(Args&&... args) {
-    return this->invoke_f(this->data_ptr.get(), std::forward<Args>(args)...);
-  }
+namespace st {
+template <typename Result, typename... Args>
+struct abstract_function {
+  virtual Result operator()(Args... args) = 0;
+  virtual abstract_function *clone() const = 0;
+  virtual ~abstract_function() = default;
 };
 
-// examples
+template <typename Func, typename Result, typename... Args>
+class concrete_function : public abstract_function<Result, Args...> {
+  Func f;
+
+ public:
+  concrete_function(const Func &x) : f(x) {}
+  Result operator()(Args... args) override { return f(args...); }
+  concrete_function *clone() const override { return new concrete_function(f); }
+};
+
+template <typename Func>
+struct func_filter {
+  typedef Func type;
+};
+// class specialization
+template <typename Result, typename... Args>
+struct func_filter<Result(Args...)> {
+  typedef Result (*type)(Args...);
+};
+
+template <typename signature>
+class function;
+
+template <typename Result, typename... Args>
+class function<Result(Args...)> {
+  abstract_function<Result, Args...> *f;
+
+ public:
+  function() : f(nullptr) {}
+  template <typename Func>
+  function(const Func &x)
+      : f(new concrete_function<typename func_filter<Func>::type, Result,
+                                Args...>(x)) {}
+
+  function(const function &rhs) : f(rhs.f ? rhs.f->clone() : nullptr) {}
+
+  function &operator=(const function &rhs) {
+    if ((&rhs != this) && (rhs.f)) {
+      auto *temp = rhs.f->clone();
+      delete f;
+      f = temp;
+    }
+    return *this;
+  }
+
+  template <typename Func>
+  function &operator=(const Func &x) {
+    auto *temp = new concrete_function<typename func_filter<Func>::type, Result,
+                                       Args...>(x);
+    delete f;
+    f = temp;
+    return *this;
+  }
+
+  Result operator()(Args... args) {
+    if (f)
+      return (*f)(args...);
+    else
+      return Result();
+  }
+
+  ~function() { delete f; }
+};
+}  // namespace st
+
+// ___________________[ Example of usage ]___________________ //
+
+int func1(double) { return 1; }
+struct Functor2 {
+  int operator()(double) { return 2; }
+};
+
 int main() {
-  int i = 0;
-  auto fn = [i](std::string const& s) mutable {
-    std::cout << ++i << ". " << s << std::endl;
-  };
-  fn("first");   // 1. first
-  fn("second");  // 2. second
+  int res = 10;
+  st::function<int(double)> f(func1);
 
-  // construct from lambda
-  ::function<void(std::string const&)> f(fn);
-  f("third");  // 3. third
+  res -= f(1.0);
+  std::cout << res << std::endl;
+  // f = Functor2{};
+  // res -= f(2.0);
+  // std::cout << res << std::endl;
 
-  // copy from another function
-  ::function<void(std::string const&)> g(f);
-  f("forth - f");  // 4. forth - f
-  g("forth - g");  // 4. forth - g
-
-  // capture and copy non-trivial types like std::string
-  std::string x("xxxx");
-  ::function<void()> h([x]() { std::cout << x << std::endl; });
-  h();
-
-  ::function<void()> k(h);
-  k();
-  return 0;
+  // auto f3 = [](double) { return 3; };
+  // f = f3;
+  // res -= f3(0);
+  // std::cout << res << std::endl;
 }
